40c30ede0f778e613ca7427f2a5471f7da2ea761
Akhil Potla <akhil4400@gmail.com>
2025-09-06 18:10:16 -0600

    Adding spring setup

---
+++ src/main/java/com/example/reports_doc_generation_api/ReportsDocGenerationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.reports_doc_generation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsDocGenerationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsDocGenerationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reports_doc_generation_api/ReportsDocGenerationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reports_doc_generation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportsDocGenerationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
